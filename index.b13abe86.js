99979f682d11dd9c4cab80f1001d564e6da2807b
Anna Onyshchenko <auonishchenko.com>
2025-03-19 19:04:04 +0300

    make build

---
+++ index.b13abe86.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".gallery"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){var n=e.target.closest("a");n&&(e.preventDefault(),t.src=n.href)})});
+//# sourceMappingURL=index.b13abe86.js.map
