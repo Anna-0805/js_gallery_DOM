af62c195332b72e9cca6c2521ad2c2f45c51455a
Anna Onyshchenko <auonishchenko.com>
2025-03-19 18:56:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gallery = document.querySelector('.gallery');
+  const mainImage = document.querySelector('main-image img');
+
+  // eslint-disable-next-line no-shadow
+  gallery.addEventListener('click', (event) => {
+    let target = event.target;
+
+    if (target.tagName === 'IMG' && target.closest('a')) {
+      event.preventDefault();
+      target = target.closest('a');
+    }
+
+    if (target.tagName === 'A') {
+      const newSrc = target.href;
+
+      mainImage.src = newSrc;
+    }
+  });
+});
